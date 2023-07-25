21f88fd0cc82248707f40f6bd24e417ca7ae896e
Andrew <noblea30@gmail.com>
2023-07-25 13:33:45 -0600

    update unique_info

--- unique_info.py
+++ unique_info.py
@@ -1,3 +1,5 @@
-profile = "C:/Users/Public/Public Documents/Chrome_details/Default"
-path = "C:/Users/Public/Public Documents/Chrome_details/chromedriver_7.exe"
+#profile = "C:/Users/Public/Public Documents/Chrome_details/Default"
+#path = "C:/Users/Public/Public Documents/Chrome_details/chromedriver_7.exe"
+profile = "Default"
+path = "chromedriver_7.exe"
 headless = False
